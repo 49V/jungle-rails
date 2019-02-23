ec74450835d678ec6bb527732416f576e7ee2bb1
Moshe Lawlor <moshelawlor@gmail.com>
2019-02-23 19:10:55 +0000

    Complete database migrations for Reviews Model that belongs to User and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
+  has_many :reviews
   belongs_to :category
 
   validates :name, presence: true
