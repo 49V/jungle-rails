5f56fe5231e7a88928acbeebdccd29ccf93c345b
Moshe Lawlor <moshelawlor@gmail.com>
2019-02-25 21:47:11 +0000

    Add email uniqueness, regex, and length validation plus associated tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,41 @@ RSpec.describe User, type: :model do
 
   it 'should not validate user with existing email' do
     
+    # Create first user with beans@beans.com
+    User.create(
+      name: "Beans",
+      email: "beans@beans.com",
+      password: "catsrule",
+      password_confirmation: "catsrule"
+    )
+    # Attempt to create second user with beans@beans.com    
+    @user = User.create(
+      name: "Beans",
+      email: "beans@beans.com",
+      password: "catsrule",
+      password_confirmation: "catsrule"
+    )
+    expect(@user.id).to be_nil
+  end
+
+  it 'should not validate user with email exceeding 255 characters' do
+    @user = User.create(
+      name: "Beans",
+      email: "test@test.com" * 20,
+      password: "catsrule",
+      password_confirmation: "catsrule"
+    )
+    expect(@user.id).to be_nil
+  end
+
+  it 'should not validate user without invalid email' do
+    @user = User.create(
+      name: "Beans",
+      email: "This is not an email",
+      password: "catsrule",
+      password_confirmation: "catsrule"
+    )
+    expect(@user.id).to be_nil
   end
 
   it 'should not validate user without email' do
