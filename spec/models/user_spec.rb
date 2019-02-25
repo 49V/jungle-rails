9202a5ffc741f7f56a3de84b99ed69aa606d8f62
Moshe Lawlor <moshelawlor@gmail.com>
2019-02-25 21:43:23 +0000

    Add initial test to User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should validate user with valid fields and matching passwords' do
+    @user = User.create(
+      name: "Beans",
+      email: "beans@bean.com",
+      password: "catsrule",
+      password_confirmation: "catsrule"
+    )
+    expect(@user.id).to be_present
+  end
+
+  it 'should not valid user with mismatched passwords' do
+    @user = User.create(
+      name: "Beans",
+      email: "beans@bean.com",
+      password: "catsrule",
+      password_confirmation: "catsdrool"
+    )
+    expect(@user.id).to be_nil
+  end
+
+  it 'should not validate user with existing email' do
+    
+  end
+
+  it 'should not validate user without email' do
+
+  end
+
+  it 'should not validate user without first name' do
+
+  end
+
+  it 'should not validate user without last name' do
+
+  end
+
 end
