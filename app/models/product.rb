a03f3ed79a4450077a737dc0d474f69e191bd0ef
Moshe Lawlor <moshelawlor@gmail.com>
2019-02-26 00:40:23 +0000

    Implement overall product review in product catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,13 @@ class Product < ActiveRecord::Base
     quantity == 0
   end
 
+  def get_average_rating
+    # Rounds the average to the nearest whole number, i.e. (4.3).round = 4, (4.6).round = 5
+    average = self.reviews.average(:rating).to_f.round()
+  end
+
+  def reviews_exist?
+    self.reviews.length > 0
+  end
+
 end
